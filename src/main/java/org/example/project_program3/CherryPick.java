c747012c03aa1537bb13eae830cb58e1cc9b6bf2
Elite <rohit.raj.mehta723@gmailcom>
2024-05-06 09:22:10 +0530

    Cherry pick master commit first time

---
+++ src/main/java/org/example/project_program3/CherryPick.java
@@ -0,0 +1,9 @@
+package org.example.project_program3;
+
+public class CherryPick {
+
+    public String print() {
+        return "This is Cherry Pick print method from master branch ";
+
+    }
+}
---
+++ src/main/java/org/example/project_program3/ProjectProgram3Application.java
@@ -0,0 +1,13 @@
+package org.example.project_program3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectProgram3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectProgram3Application.class, args);
+    }
+
+}
